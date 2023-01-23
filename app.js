71e291aecdd817494aa635ed11a92390396f514e
DkidOD <a.khrishchanovich@gmail.com>
2023-01-23 12:18:41 +0200

    Basic arithmetics operators

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-console.log("Hello world! I'm a new JavaScript learner !!! ;)")
+// console.log("Hello world! I'm a new JavaScript learner !!! ;)")
+
+/*
+Name of variables
+
+Good->
+let userName = 'Valentin';
+let myClass8 = 8;
+let _name = 'Anton';
+let $myVar = 'var';
+
+Not recommended ->
+let user_name = 'Valentin'
+let UserName = 'Valentin'
+
+Prohibited ->
+let 8myClass = 8;
+let my-Var = 'var';
+let const = '1';
+
+Deprecated variable 'var' 
+We can use 'let' and 'const' instead 'var'
+
+*/
+
+/*
+let a = 1
+console.log(a);
+a = 'test';
+console.log(a);
+*/
+
+// Inline comment
+
+/*
+Two line
+comment
+*/
+
+// Basic arithmetics operators
+
+const width = 10;
+const height = 5;
+const square = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2
+console.log( division );
